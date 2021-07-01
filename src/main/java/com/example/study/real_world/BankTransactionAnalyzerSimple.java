30720b9da24ef135d731d608eca5795646998796
Leedo <leedo@deali.net>
2021-07-01 23:16:18 +0900

    step2

--- src/main/java/com/example/study/real_world/BankTransactionAnalyzerSimple.java
+++ src/main/java/com/example/study/real_world/BankTransactionAnalyzerSimple.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -18,9 +20,13 @@ public class BankTransactionAnalyzerSimple {
         final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         for (final String line : lines) {
             final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]);
-            total += amount;
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+
+            if (date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
         }
-        System.out.println("The total for all transactions is " + total);
+        System.out.println("The total for all transactions in january is " + total);
     }
 }
