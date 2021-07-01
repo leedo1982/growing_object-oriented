adcbfd2618ae828d59ea60bd0a6ac0c1d221c650
Leedo <leedo@deali.net>
2021-07-01 23:14:42 +0900

    step1

---
+++ src/main/java/com/example/study/real_world/BankTransactionAnalyzerSimple.java
@@ -0,0 +1,26 @@
+package com.example.study.real_world;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class BankTransactionAnalyzerSimple {
+    public static final String RESOURCES = "src/main/resources/";
+
+    public static void main(final String... args) throws IOException {
+
+        final Path path = Paths.get(RESOURCES + args[0]);
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        for (final String line : lines) {
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount;
+        }
+        System.out.println("The total for all transactions is " + total);
+    }
+}
